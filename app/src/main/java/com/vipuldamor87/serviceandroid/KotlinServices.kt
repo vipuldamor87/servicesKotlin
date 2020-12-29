48bba81e6579d8e7c3844ebe61529058c0775670
vipuldamor87 <vipuldamor26@gmail.com>
2020-12-29 16:45:31 +0530

    commit

--- app/src/main/java/com/vipuldamor87/serviceandroid/KotlinServices.kt
+++ app/src/main/java/com/vipuldamor87/serviceandroid/KotlinServices.kt
@@ -5,21 +5,23 @@ import android.content.Intent
 import android.os.IBinder
 import android.util.Log
 import android.widget.Toast
-import java.lang.Thread.sleep
 
 class KotlinServices : Service() {
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         onTaskRemoved(intent)
-
-                Log.d("TAG", "This service is running ")
-
-        return START_STICKY
+        //using thread will reduce lag in UI
+        val thread = Thread {
+            Log.d("TAG", "This service is running ")
+        }
+        thread.start()
+               return START_STICKY
     }
 
     override fun onTaskRemoved(rootIntent: Intent?) {
         val restartServiceIntent = Intent(applicationContext, this.javaClass)
         restartServiceIntent.setPackage(packageName)
         startService(restartServiceIntent)
+
         super.onTaskRemoved(rootIntent)
     }
     override fun onDestroy() {
