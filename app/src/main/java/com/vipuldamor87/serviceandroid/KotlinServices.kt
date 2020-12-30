8757e0e4a7a54b51c0da6fa6bf7cd6aec55769e1
vipuldamor87 <vipuldamor26@gmail.com>
2020-12-30 15:17:50 +0530

    commit

--- app/src/main/java/com/vipuldamor87/serviceandroid/KotlinServices.kt
+++ app/src/main/java/com/vipuldamor87/serviceandroid/KotlinServices.kt
@@ -1,20 +1,61 @@
 package com.vipuldamor87.serviceandroid
 
+import android.Manifest
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Bundle
 import android.os.IBinder
 import android.util.Log
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import androidx.core.app.ActivityCompat
+import java.lang.Thread.sleep
+import java.util.*
+import kotlin.concurrent.thread
 
 class KotlinServices : Service() {
+    lateinit var lm : LocationManager
+    lateinit var loc: Location
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+
+        lm = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+      try{ loc = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER)}
+       catch(e :Exception)
+       {
+           Log.d("error","not working")
+       }
+        /*
+        var ll = object : LocationListener {
+            override fun onLocationChanged(location: Location?) {
+                revereGeoCode(location)
+            }
+
+            override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onProviderEnabled(provider: String?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onProviderDisabled(provider: String?) {
+                TODO("Not yet implemented")
+            }
+
+        }*/
         onTaskRemoved(intent)
         //using thread will reduce lag in UI
-        val thread = Thread {
-            Log.d("TAG", "This service is running ")
-        }
-        thread.start()
-               return START_STICKY
+        Log.d("TAG", "This service is running ")
+        revereGeoCode(loc)
+        return START_STICKY
         //return START_REDELIVER_INTENT to reinstatiate the service when application is removed from recents
     }
 
@@ -22,7 +63,6 @@ class KotlinServices : Service() {
         val restartServiceIntent = Intent(applicationContext, this.javaClass)
         restartServiceIntent.setPackage(packageName)
         startService(restartServiceIntent)
-
         super.onTaskRemoved(rootIntent)
     }
     override fun onDestroy() {
@@ -35,4 +75,14 @@ class KotlinServices : Service() {
     override fun onBind(intent: Intent?): IBinder? {
         return null
     }
+
+
+    private fun revereGeoCode(loc: Location?) {
+        var gc = Geocoder(this, Locale.getDefault())
+        var addresses = gc.getFromLocation(loc!!.latitude,loc!!.longitude,2)
+        var address = addresses.get(0)
+        Toast.makeText(applicationContext,"${address.locality}",Toast.LENGTH_LONG).show()
+        Log.d("Mylocation","${address.locality}")
+    }
+
 }
--- app/src/main/java/com/vipuldamor87/serviceandroid/MainActivity.kt
+++ app/src/main/java/com/vipuldamor87/serviceandroid/MainActivity.kt
@@ -1,20 +1,52 @@
 package com.vipuldamor87.serviceandroid
 
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         title= "KotlinApp"
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this, arrayOf(
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.ACCESS_FINE_LOCATION),111)
+        }
+
+
+        //if(ActivityCompat.checkSelfPermission(this,Manifest.permission.access))
+
         btnService.setOnClickListener{
             startService(Intent(applicationContext, KotlinServices::class.java))
         }
         btnServiceStop.setOnClickListener{
             applicationContext.stopService(Intent(applicationContext, KotlinServices::class.java))
         }
+
+
+
     }
+
 }
