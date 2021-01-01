69f399d99d5f5fd8e37ad3504fe3222ff2dc980d
vipuldamor87 <vipuldamor26@gmail.com>
2021-01-01 12:55:30 +0530

    commit

--- app/src/main/java/com/vipuldamor87/foregroundservice/Worker/PeriodicWorker.kt
+++ app/src/main/java/com/vipuldamor87/foregroundservice/Worker/PeriodicWorker.kt
@@ -14,13 +14,19 @@ class PeriodicWorker(context: Context, params: WorkerParameters) : Worker(contex
 
     override fun doWork(): Result {
         try {
-            val count = inputData.getInt(MainActivity.KEY_COUNT_VALUE,0)
+            //val count = inputData.getInt(MainActivity.KEY_COUNT_VALUE,0)
             for (i in 0..30) {
                 Log.d("Worker", "uploading")
             }
-            /*val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
+            val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
                 .build()
-            WorkManager.getInstance(applicationContext).enqueue(uploadRequest)*/
+            val parallelWorker = mutableListOf<OneTimeWorkRequest>()
+            parallelWorker.add(uploadRequest)
+            WorkManager.getInstance(applicationContext)
+                .beginWith(parallelWorker)
+                    .enqueue()
+
+
             val time = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
             val currentDate = time.format(Date())
             Log.d("PeriodicWorker","$currentDate")
